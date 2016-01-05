8ddc1f322d754873e413b190512e6d38a56acaef
Paul Beck <pb@teamgeist-medien.de>
2016-01-05 16:35:36 +0100

    Tes

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,6 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'typo3' => '6.2.0-7.99.99',
             'beuser' => '6.2.0-7.99.99',
+            test
         ),
         'conflicts' => array(),
         'suggests' => array(),
