7a81284d367f747bfc0de4d3c61b9bc3f6c1ca0c
Benjamin Serfhos <serfhos@gmail.com>
2016-07-07 13:30:31 +0200

    [TASK] Update compatibility constraint for TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => 'Rotterdam School of Management, Erasmus University',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-8.99.99',
+            'typo3' => '7.6.0-8.99.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
